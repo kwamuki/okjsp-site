90f44bd270b3d4a29b3b6c19be339a7c733fd2b9
kwamuki <kwamuki@gmail.com>
2013-04-23 21:48:40 +0900

    한국생산성본부 배너광고 소스수정

--- src/main/webapp/js/banner.js
+++ src/main/webapp/js/banner.js
@@ -49,7 +49,8 @@ show : function() {
 },
 
 showAside : function() {
-	document.writeln('<a href="/f.jsp?http://www.eclipse.org/donate/" target="_blank"><img src="http://eclipse.org/donate/images/friendslogo200.jpg" style="width:150px"></a>');
+	'document.writeln('<a href="/f.jsp?http://www.eclipse.org/donate/" target="_blank"><img src="http://eclipse.org/donate/images/friendslogo200.jpg" style="width:150px"></a>');
+	document.writeln('<a href="/f.jsp?http://www.ddddddd.com/" target="_blank"><img src="/images/ddd.gif"></a>');
     document.writeln("<ul>");
     for(var i = 0; i < this.aside.length; i++) {
         document.writeln("<li>"+this.aside[i].tag+"</li>");
